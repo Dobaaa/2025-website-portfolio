b249c6a47d8d53a85989f4d74171c0c2078088fd
Your Name <you@example.com>
2025-07-03 18:44:06 +0300

    first version

---
+++ data/index.ts
@@ -0,0 +1,220 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to download my CV ?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Saboba(Booking Web App)",
+    des: "Developing a full stack responsive booking reservation web App that Used for book workers it was a big project had a lot of pages and logics and it had fine details",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "3s System [React JS] (workspace management system )",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "Pentola restaurant website",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Sales Data Visualization Dashboard",
+    des: "A full-stack web application for managing and visualizing quarterly sales data with interactive 3D charts. Built with React.js, Three.js, Laravel, and MySQL.",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Ahmed did excellent work. He has good communication skills and very responsive. Would definitely hire him again.",
+    name: "Murat Gultepe",
+    title: "Director of Solar Company",
+  },
+  {
+    quote:
+      "Collaborating with Ahmed J was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project Ahmed's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Softa Solutions Company ",
+    title: "CEO of Softa Solutions Company",
+  },
+  {
+    quote:
+      "Working with Ahmed  was a seamless experience. His attention to detail, creative approach, and ability to communicate effectively made the entire process smooth and enjoyable .Ahmed brings passion and technical brilliance to every project. Highly recommended for anyone aiming to build a modern, high-performing website.",
+    name: "Muhammed Hussien ",
+    title: "Head of Digital Strategy at Nova Solutions",
+  },
+  {
+    quote:
+      "Collaborating with Lina on our front-end revamp was a fantastic experience. Her mastery of modern frameworks, eye for UI/UX detail, and ability to translate designs into seamless, responsive interfaces truly elevated our product. If you're looking for a front-end developer who combines technical skill with design sensitivity, Lina is the one",
+    name: "Omar El-Masry",
+    title: "Lead Product Manager at BrightLoop Studios",
+  },
+  {
+    quote:
+      "I had the pleasure of partnering with Ahmed  on a recent project, and I was genuinely impressed by his commitment and technical know-how. Ahmed consistently delivered beyond expectations and brought fresh, strategic thinking to the table. He's an excellent choice if you're looking to enhance your online presence.",
+    name: "Daniel Monroe",
+    title: "Marketing Director at PixelWave Medias",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer ",
+    desc: "I worked on serval large React JS system and , also worked on integrating backend APIs and creating a responsive web apps.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Software Engineer",
+    desc: "Developed and maintained modern, responsive web applications using React.js and other front-end & backend  technologies. Focused on creating clean, scalable code and delivering exceptional user experiences across various browsers and devices.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Working with a different clients from different countries and cultures in freelancer.com andUpwork.com , So I made more than type of project.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Dobaaa",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://github.com/Dobaaa",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/ahmed-jamal-3a509b220/",
+  },
+];
